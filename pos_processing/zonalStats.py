6949b5ae828e105977d2fde4d9be02b8d9ccb930
PenghuaLI <MoonLee003@outlook.com>
2017-07-13 15:07:00 +0200

    li

--- pos_processing/zonalStats.py
+++ pos_processing/zonalStats.py
@@ -146,33 +146,33 @@ def main(input_zone_polygon, input_value_raster):
 #    print 'Returns for each feature a dictionary item (FID) with the statistical values in the following order: Average, Mean, Medain, Standard Deviation, Variance'
 #    print main( sys.argv[1], sys.argv[2] )
 
-shpfile = r'E:\Penghua\data\Etna\2014.06.22\TET\ac_results_1.04_MIR_TIR\POLY.shp'
-
-rasterfile = r'E:\Penghua\data\Etna\2014.06.22\TET\ac_results_1.04_MIR_TIR\FBI_TET1_20140622T232052_20140622T232155_L2_002589_WHM_cobined_MIR_TIR_tem_MIR_only.tif'
-
-if os.path.exists(os.path.join(os.path.split(shpfile)[0], 'temp')) == False:
-    
-    os.mkdir(os.path.join(os.path.split(shpfile)[0], 'temp'))
-#   
-sta = main(shpfile, rasterfile)
-
-shutil.rmtree(os.path.join(os.path.split(shpfile)[0], 'temp'))
-
-#shpFile = r'E:\Penghua\data\Etna\shapefiles'
+#shpfile = r'E:\Penghua\data\Etna\2014.06.22\TET\ac_results_1.04_MIR_TIR\POLY.shp'
 #
-#shp = []
+#rasterfile = r'E:\Penghua\data\Etna\2014.06.22\TET\ac_results_1.04_MIR_TIR\FBI_TET1_20140622T232052_20140622T232155_L2_002589_WHM_cobined_MIR_TIR_tem_MIR_only.tif'
 #
-#os.chdir(shpFile)
-#
-#for files in os.listdir(shpFile):
+#if os.path.exists(os.path.join(os.path.split(shpfile)[0], 'temp')) == False:
 #    
-#    if files.endswith('.shp') and 'rect' in files:
-#        
-#        shp.append(os.path.abspath(files)) 
+#    os.mkdir(os.path.join(os.path.split(shpfile)[0], 'temp'))
+#   
+#sta = main(shpfile, rasterfile)
 #
-#sourFile = r'E:\Penghua\data\Etna'
-#
-#os.chdir(sourFile)
+#shutil.rmtree(os.path.join(os.path.split(shpfile)[0], 'temp'))
+
+shpFile = r'E:\Penghua\data\Etna\shapefiles'
+
+shp = []
+
+os.chdir(shpFile)
+
+for files in os.listdir(shpFile):
+    
+    if files.endswith('.shp') and 'rect' in files:
+        
+        shp.append(os.path.abspath(files)) 
+
+sourFile = r'E:\Penghua\data\Etna'
+
+os.chdir(sourFile)
 
 #filename = xlwt.Workbook()
 #
@@ -194,51 +194,51 @@ shutil.rmtree(os.path.join(os.path.split(shpfile)[0], 'temp'))
 #
 #count = 1
 
-#scale_factor = ['1.00', '1.05', '1.10', '1.15', '1.20']
+scale_factor = ['1.00', '1.05', '1.10', '1.15', '1.20']
+
+sc_mir = [[], [], [], [], []]
+sc_tir = [[], [], [], [], []]
+time = []
 #
-#sc_mir = [[], [], [], [], []]
-#sc_tir = [[], [], [], [], []]
-#time = []
-#
-#for i in range(len(scale_factor)):
-#    
-#    #print scale_factor[i]
-#    
-#    
-#    for files in os.listdir(sourFile):
-#        
-#        if '0' in files:
+for i in range(len(scale_factor)):
+    
+    #print scale_factor[i]
+    
+    
+    for files in os.listdir(sourFile):
+        
+        if '0' in files:
+            
+            if i == 0:
+                
+                time.append(files)
+            
+            #print files
+            
+#            sheet1.write(0, count, files)
 #            
-#            if i == 0:
-#                
-#                time.append(files)
-#            
-#            #print files
-#            
-##            sheet1.write(0, count, files)
-##            
-##            sheet2.write(0, count, files)
-#            
-#            ac_folder = os.path.join(os.path.abspath(files), r'TET\ac_results_%s\compared' %scale_factor[i])
-#            
-#            
-#            if os.path.exists(os.path.join(os.path.abspath(files), r'TET\ac_results_%s\compared' %scale_factor[i])) == True:
-#            
-#                ac_results = os.path.join(os.path.abspath(files), r'TET\ac_results_%s\compared' %scale_factor[i])
-#                
-#                if os.path.exists(os.path.join(ac_results, 'temp')) == False:
-#    
-#                    os.mkdir(os.path.join(ac_results, 'temp'))
-#            
-#                for fil in os.listdir(ac_results):
-#                
-#                    if fil.endswith('.tif') and 'MIR' in fil:
-#                    
-#                        TET_tem_MIR = os.path.join(ac_results, fil)
-#                        
-#                    if fil.endswith('.tif') and 'TIR' in fil:
-#                    
-#                        TET_tem_TIR = os.path.join(ac_results, fil)  
+#            sheet2.write(0, count, files)
+            
+            ac_folder = os.path.join(os.path.abspath(files), r'TET\ac_results_%s\compared' %scale_factor[i])
+            
+            
+            if os.path.exists(os.path.join(os.path.abspath(files), r'TET\ac_results_%s\compared' %scale_factor[i])) == True:
+            
+                ac_results = os.path.join(os.path.abspath(files), r'TET\ac_results_%s\compared' %scale_factor[i])
+                
+                if os.path.exists(os.path.join(ac_results, 'temp')) == False:
+    
+                    os.mkdir(os.path.join(ac_results, 'temp'))
+            
+                for fil in os.listdir(ac_results):
+                
+                    if fil.endswith('.tif') and 'MIR' in fil:
+                    
+                        TET_tem_MIR = os.path.join(ac_results, fil)
+                        
+                    if fil.endswith('.tif') and 'TIR' in fil:
+                    
+                        TET_tem_TIR = os.path.join(ac_results, fil)  
             
 #            MIR_rect2 = main(shp[1], TET_tem_MIR)
 #            
@@ -267,35 +267,122 @@ shutil.rmtree(os.path.join(os.path.split(shpfile)[0], 'temp'))
 #            
 #            count = count + 1                         
                 
-#                sc_mir[i].append((main(shp[1], TET_tem_MIR)[0]+main(shp[3], TET_tem_MIR)[0]+main(shp[4], TET_tem_MIR)[0]) / 3.0)
-#                
-#                sc_tir[i].append((main(shp[1], TET_tem_TIR)[0]+main(shp[3], TET_tem_TIR)[0]+main(shp[4], TET_tem_TIR)[0]) / 3.0)
-#                
-#                if os.path.exists(os.path.join(ac_results, 'temp')) == True:
-#                    
-#                    shutil.rmtree(os.path.join(ac_results, 'temp'))
-#                
-#            else:
-#                
-#                sc_mir[i].append(0.0)
-#                sc_tir[i].append(0.0)
+                sc_mir[i].append((main(shp[1], TET_tem_MIR)[0]+main(shp[3], TET_tem_MIR)[0]+main(shp[4], TET_tem_MIR)[0]) / 3.0)
+                
+                sc_tir[i].append((main(shp[1], TET_tem_TIR)[0]+main(shp[3], TET_tem_TIR)[0]+main(shp[4], TET_tem_TIR)[0]) / 3.0)
+                
+                if os.path.exists(os.path.join(ac_results, 'temp')) == True:
+                    
+                    shutil.rmtree(os.path.join(ac_results, 'temp'))
+                
+            else:
+                
+                sc_mir[i].append(0.0)
+                sc_tir[i].append(0.0)
                 
 
 #zero = numpy.zeros([1,16])
+#fig1, ax1 = plt.subplots()
+##plt.xticks(range(16), time, rotation=30, fontsize=4)
+#p1, = ax1.plot(sc_mir[0], 'yo-')
+#p2, = ax1.plot(sc_mir[1], 'go-')
+#p3, = ax1.plot(sc_mir[2], 'ro-')
+#p4, = ax1.plot(sc_mir[3], 'ko-')
+#p5, = ax1.plot(sc_mir[4], 'co-')
+#p6, = ax1.plot(zero[0], 'b--')
+#ax1.set_title('Temperature Differences for Etna Scenes in MIR band')
+#ax1.set_xlabel('time')
+#ax1.set_ylabel('Temperature Differences [deg]')
+#ax1.set_xticks(range(16))
+#ax1.set_xticklabels(time, rotation=30, fontsize=5)
+#plt.legend([p1,p2,p3,p4,p5], ['scale factor 1.00','scale factor 1.05','scale factor 1.10','scale factor 1.15','scale factor 1.20'],prop={'size':7})
+#fig1.tight_layout()
+#plt.grid()
+#fig1.savefig(os.path.join(r'E:\Penghua\results\ComSST\Etna', r'Etna_scf_mir.png'), dpi=200)
+#plt.show()
+
+#zero = numpy.zeros([1,16])
 #plt.xticks(range(16), time, rotation=30, fontsize=4)
-#p1, = plt.plot(sc_mir[0], 'yo-')
-#p2, = plt.plot(sc_mir[1], 'go-')
-#p3, = plt.plot(sc_mir[2], 'ro-')
-#p4, = plt.plot(sc_mir[3], 'ko-')
-#p5, = plt.plot(sc_mir[4], 'co-')
+#p1, = plt.plot(sc_tir[0], 'yo-')
+#p2, = plt.plot(sc_tir[1], 'go-')
+#p3, = plt.plot(sc_tir[2], 'ro-')
+#p4, = plt.plot(sc_tir[3], 'ko-')
+#p5, = plt.plot(sc_tir[4], 'co-')
 #p6, = plt.plot(zero[0], 'b--')
 #plt.title('Temperature Differences for Etna Scenes in MIR band')
 #plt.xlabel('time')
 #plt.ylabel('Temperature Differences [deg]')
 #legend = plt.legend([p1,p2,p3,p4,p5], ['scale factor 1.00','scale factor 1.05','scale factor 1.10','scale factor 1.15','scale factor 1.20'],prop={'size':7})
-#
+#plt.grid()
+#plt.savefig(os.path.join(r'E:\Penghua\results\ComSST\Etna', r'Etna_scf_tir.png'), dpi=200)
 #plt.show()
-#plt.savefig(os.path.join(sourFile, r'te.png'), dpi=200)
 
+sc = []
+
+index = []
+
+b_sc = []
+
+for i in range(len(sc_mir[0])):
+    
+    temp = []
+    
+    for j in range(len(sc_mir)):
+        
+        if sc_mir[j][i] == 0:
+            
+            temp.append(10)
+            
+        else:
+            
+            temp.append(abs(sc_mir[j][i]))
+    
+    sc.append(min(temp))
+    
+    index.append(temp.index(min(temp)))
+    
+    b_sc.append(scale_factor[temp.index(min(temp))])
+
+diff_tem = []
+
+for sc in b_sc:
+    
+    diff_tem.append(sc_mir[scale_factor.index(sc)][b_sc.index(sc)])
+    
+
+fig, ax1 = plt.subplots()
+p1, = ax1.plot(b_sc, 'ro-')
+ax1.set_title('Best scale factor for each scene and corresponding $\Delta$T')
+ax1.set_ylabel('best scale factor for each scene', color='r')
+ax1.tick_params('y', colors='r')
+
+ax2 = ax1.twinx()
+p2, = ax2.plot(diff_tem, 'bo--',markerfacecolor='none')
+ax2.set_ylabel('corresponding temperature', color='b')
+ax2.tick_params('y', colors='b')
+ax1.set_xlabel('time')
+ax1.set_xticks(range(16))
+ax1.set_xticklabels(time, rotation=30, fontsize=5)
+fig.tight_layout()
+plt.grid()
+plt.legend([p1,p2], [r'best scale factor', r'corresponding $\Delta$T'],prop={'size':7})
+fig.savefig(os.path.join(r'E:\Penghua\results\ComSST\Etna', r'Etna_bsc&tem_mir.png'), dpi=200)
+plt.show()     
+
+fig2, ax1 = plt.subplots()
+p1, = ax1.plot(diff_tem, 'ro-',markerfacecolor='none')
+p2, = ax1.plot(sc_mir[1], 'go--',markerfacecolor='none')
+ax1.plot(8, 0, 'rx', markersize=8)
+p3, = ax1.plot(sc_mir[2], 'bo--',markerfacecolor='none')
+ax1.set_title('Temperature Differences for Etna Scenes in MIR band')
+ax1.set_ylabel('Temperature Differences [deg]')
+ax1.set_xlabel('time')
+ax1.set_xticks(range(16))
+ax1.set_xticklabels(time, rotation=30, fontsize=5)
+fig.tight_layout()
+plt.legend([p1,p2,p3], [r'smallest $\Delta$T',r'$\Delta$T for scale factor 1.05','$\Delta$T for scale factor 1.10'],prop={'size':7})
+plt.grid()
+fig2.savefig(os.path.join(r'E:\Penghua\results\ComSST\Etna', r'Etna_bsc&temCom_mir.png'), dpi=200)
+plt.show()
 
 #filename.save(os.path.join(sourFile, 'scale_factor_1.00.xls'))
